d90588a1c235bad97c031363eee93106d600a8e2
prizzaflutter <prizzaflutter@gmail.com>
2026-01-01 16:54:56 +0100

    final push

--- ex02/Animal.hpp
+++ ex02/Animal.hpp
@@ -11,7 +11,7 @@ class Animal {
         Animal(const Animal& other);
         Animal& operator=(const Animal& other);
         virtual ~Animal();
-        virtual void makeSound() const = 0;
+        virtual void makeSound() const = 0; 
         std::string getType() const;
 };
 
